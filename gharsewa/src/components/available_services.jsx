e90b063fe8f76b653b406ff26d21e770fa664800
yakhha012 <57519719+yakhha012@users.noreply.github.com>
2021-06-11 13:19:56 +0545

    home page

---
+++ gharsewa/src/components/available_services.jsx
@@ -0,0 +1,66 @@
+import React, { Fragment } from 'react';
+import Grid from '@material-ui/core/Grid';
+import ServicesCard from './cards.jsx';
+import coverImage  from './services.jpg'
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+    title: {
+        textAlign: 'left',
+        letterSpacing:'3px',
+    },
+    root: {
+        marginTop: "4%",
+    }
+}));
+export default function AvailableService(){
+    const classes = useStyles();
+
+    return(
+        <React.Fragment>
+        <div><h3 className={classes.title}>AVAILABLE SERVICES:</h3></div>
+
+        <div className={classes.root}>
+        <Grid container spacing={2}>
+
+        <Grid item xs={3}>
+        <ServicesCard image ={coverImage} title="Plummer"/>
+        </Grid>
+        <Grid item xs={3}>
+        <ServicesCard image ={coverImage} title="Electrical" />
+        </Grid>
+        <Grid item xs={3}>
+        <ServicesCard image ={coverImage} title="Electrical" />
+        </Grid>
+        <Grid item xs={3}>
+        <ServicesCard image ={coverImage} title="Electrical" />
+        </Grid>
+
+        </Grid>
+        </div>
+        
+{/* ---------------------------------------------------------------------- */}
+        <div className={classes.root}>
+        <Grid container spacing={2}>
+
+        <Grid item xs={3}>
+        <ServicesCard image ={coverImage} title="Plummer" />
+        </Grid>
+        <Grid item xs={3}>
+        <ServicesCard image ={coverImage} title="Electrical" />
+        </Grid>
+        <Grid item xs={3}>
+        <ServicesCard image ={coverImage} title="Electrical" />
+        </Grid>
+        <Grid item xs={3}>
+        <ServicesCard image ={coverImage} title="Electrical" />
+        </Grid>
+
+        </Grid>
+        </div>
+
+        </React.Fragment>
+        
+    );
+}
---
+++ gharsewa/src/components/popular_services.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { Card, CardContent } from '@material-ui/core';
+import coverImage  from './services.jpg'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        height:"100%",
+        width:"95%",
+        // marginLeft:"10%",
+        boxShadow: 'rgba(255, 0, 0, 0.117647) 0px 1px 6px, rgba(255, 0, 0, 0.117647) 0px 1px 4px',
+        borderRadius: 14,
+        // backgroundColor: theme.palette.primary.light,
+        // color: theme.palette.primary.contrastText,
+    },
+    titles: {
+      textAlign:"left",
+      letterSpacing:'3px',
+    },
+    services: {
+        textAlign:"left",
+        marginLeft:"6%",
+        marginTop:"4%",
+        letterSpacing:'1px',
+    },
+    media: {
+        height: 330,
+        
+    },
+    
+  }));
+
+  
+export default function PopularServices(props){
+    const classes = useStyles();
+
+    const listServices = props.popularServices;
+    const listItems = listServices.map((listService) => 
+        // <p><a href="#" style={{textDecoration:"none"}}>{listService}</a></p>
+        <p>{listService}</p>
+    )
+
+    return(
+        <Grid container>
+            <Grid item xs={0} sm={1}/>
+            <Grid item xs={5} sm={2}>
+               <Card className={classes.root} variant="outlined">
+                <CardContent>
+                <Typography className={classes.titles} variant="h6">
+                   Our Services:
+                </Typography>
+                <Typography className={classes.services}>{listItems}</Typography>
+                </CardContent>
+               </Card>
+               
+            </Grid>
+
+            <Grid item xs={7} sm={8} >
+                <img src={coverImage} height="100%" width="100%" className={classes.root}/>
+            </Grid>
+            <Grid item xs={0} sm={1} />          
+        </Grid>
+    );
+}
